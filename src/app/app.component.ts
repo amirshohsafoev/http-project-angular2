53e488e17d2df25012470e8b2d7968cda12ef803
amirshohsafoev <amirsafoev0@gmail.com>
2019-08-21 22:46:33 -0400

    outputting posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import {Post} from './post.model'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -53,7 +53,7 @@ export class AppComponent implements OnInit {
       return postsArray
     }))
     .subscribe(posts => {
-      console.log(posts[0])
+      this.loadedPosts = posts
     })
   }
 }
